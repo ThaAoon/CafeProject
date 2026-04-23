4d444b7446d52f4e554c8454ae94024cca09453b
ThaAoon <thoon6665@gmail.com>
2026-04-23 21:28:34 +0700

    Update models for SQLite compatibility and sync data from SQL Server

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 # Create your models here.
     
 class Categories(models.Model):
-    category_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
-    category_name = models.CharField(max_length=100, db_collation='Thai_CS_AI')
+    category_id = models.CharField(primary_key=True, max_length=6)
+    category_name = models.CharField(max_length=100)
 
     class Meta:
         managed = True
@@ -12,10 +12,10 @@ class Categories(models.Model):
 
 
 class Customers(models.Model):
-    customer_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
-    cus_name = models.CharField(max_length=100, db_collation='Thai_CS_AI', blank=True, null=True)
-    cus_phone = models.CharField(unique=True, max_length=20, db_collation='Thai_CS_AI')
-    cus_email = models.CharField(max_length=100, db_collation='Thai_CS_AI', blank=True, null=True)
+    customer_id = models.CharField(primary_key=True, max_length=6)
+    cus_name = models.CharField(max_length=100, blank=True, null=True)
+    cus_phone = models.CharField(unique=True, max_length=20)
+    cus_email = models.CharField(max_length=100, blank=True, null=True)
     member_points = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
 
@@ -30,13 +30,13 @@ class Employees(models.Model):
         ('Assistant manager', 'Assistant manager'),
         ('Sales', 'Sales'),
     ]
-    employee_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
-    emp_name = models.CharField(max_length=100, db_collation='Thai_CS_AI')
-    position = models.CharField(max_length=100, choices=POSITION_CHOICES, db_collation='Thai_CS_AI', blank=True, null=True)
-    emp_phone = models.CharField(max_length=50, db_collation='Thai_CS_AI', blank=True, null=True)
+    employee_id = models.CharField(primary_key=True, max_length=6)
+    emp_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100, choices=POSITION_CHOICES, blank=True, null=True)
+    emp_phone = models.CharField(max_length=50, blank=True, null=True)
     hire_date = models.DateField(blank=True, null=True)
     salary = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    emp_status = models.CharField(max_length=50, db_collation='Thai_CS_AI', blank=True, null=True)
+    emp_status = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
         managed = True
@@ -44,14 +44,14 @@ class Employees(models.Model):
 
 
 class Ingredients(models.Model):
-    ingredient_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
+    ingredient_id = models.CharField(primary_key=True, max_length=6)
     supplier = models.ForeignKey('Suppliers', models.DO_NOTHING, blank=True, null=True)
-    ingredient_name = models.CharField(max_length=150, db_collation='Thai_CS_AI')
-    unit = models.CharField(max_length=50, db_collation='Thai_CS_AI', blank=True, null=True)
+    ingredient_name = models.CharField(max_length=150)
+    unit = models.CharField(max_length=50, blank=True, null=True)
     stock_qty = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     min_qty = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     expiry_date = models.DateField(blank=True, null=True)
-    ingredirent_status = models.CharField(max_length=20, db_collation='Thai_CS_AI', blank=True, null=True)
+    ingredirent_status = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
         managed = True
@@ -65,7 +65,7 @@ class OrderDetails(models.Model):
     quantity = models.IntegerField(blank=True, null=True)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     subtotal = models.DecimalField(max_digits=21, decimal_places=2, blank=True, null=True)
-    note = models.CharField(max_length=255, db_collation='Thai_CS_AI', blank=True, null=True)
+    note = models.CharField(max_length=255, blank=True, null=True)
     sweetness = models.IntegerField(default=100, blank=True, null=True)
 
     class Meta:
@@ -74,13 +74,13 @@ class OrderDetails(models.Model):
 
 
 class Orders(models.Model):
-    order_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
+    order_id = models.CharField(primary_key=True, max_length=6)
     customer = models.ForeignKey(Customers, models.DO_NOTHING, blank=True, null=True)
     employee = models.ForeignKey(Employees, models.DO_NOTHING)
     order_datetime = models.DateTimeField(blank=True, null=True)
-    order_type = models.CharField(max_length=20, db_collation='Thai_CS_AI', blank=True, null=True)
+    order_type = models.CharField(max_length=20, blank=True, null=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    order_status = models.CharField(max_length=20, db_collation='Thai_CS_AI', blank=True, null=True)
+    order_status = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
         managed = True
@@ -91,9 +91,9 @@ class Payments(models.Model):
     payment_id = models.AutoField(primary_key=True)
     order = models.ForeignKey(Orders, models.DO_NOTHING)
     payment_datetime = models.DateTimeField(blank=True, null=True)
-    payment_method = models.CharField(max_length=50, db_collation='Thai_CS_AI', blank=True, null=True)
+    payment_method = models.CharField(max_length=50, blank=True, null=True)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_status = models.CharField(max_length=20, db_collation='Thai_CS_AI', blank=True, null=True)
+    payment_status = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
         managed = True
@@ -101,13 +101,13 @@ class Payments(models.Model):
 
 
 class Products(models.Model):
-    product_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
+    product_id = models.CharField(primary_key=True, max_length=6)
     category = models.ForeignKey(Categories, models.DO_NOTHING)
-    product_name = models.CharField(max_length=150, db_collation='Thai_CS_AI')
+    product_name = models.CharField(max_length=150)
     description = models.TextField(db_collation='Thai_CS_AI', blank=True, null=True)  # This field type is a guess.
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    size = models.CharField(max_length=50, db_collation='Thai_CS_AI', blank=True, null=True)
-    is_active = models.CharField(max_length=10, db_collation='Thai_CS_AI')
+    size = models.CharField(max_length=50, blank=True, null=True)
+    is_active = models.CharField(max_length=10)
 
     class Meta:
         managed = True
@@ -115,19 +115,19 @@ class Products(models.Model):
 
 
 class Suppliers(models.Model):
-    supplier_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
-    supplier_name = models.CharField(max_length=150, db_collation='Thai_CS_AI')
-    contact_name = models.CharField(max_length=100, db_collation='Thai_CS_AI', blank=True, null=True)
-    phone = models.CharField(max_length=20, db_collation='Thai_CS_AI', blank=True, null=True)
-    email = models.CharField(max_length=100, db_collation='Thai_CS_AI', blank=True, null=True)
-    address = models.CharField(max_length=255, db_collation='Thai_CS_AI', blank=True, null=True)
+    supplier_id = models.CharField(primary_key=True, max_length=6)
+    supplier_name = models.CharField(max_length=150)
+    contact_name = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = True
         db_table = 'suppliers'
 
 class Recipes(models.Model):
-    recipe_id = models.CharField(primary_key=True, max_length=6, db_collation='Thai_CS_AI')
+    recipe_id = models.CharField(primary_key=True, max_length=6)
     product = models.ForeignKey(Products, models.DO_NOTHING)
     ingredient = models.ForeignKey(Ingredients, models.DO_NOTHING)
     quantity_used = models.DecimalField(max_digits=10, decimal_places=2)
